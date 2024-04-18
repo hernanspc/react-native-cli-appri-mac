1485291b1a042f754a45db354735e11970f4e020
Hernan Pormachi <encora-hernan.pormachi@mibanco.com.pe>
2024-04-18 12:35:54 -0500

    rimac app package

--- src/pages/home/HomeScreen.tsx
+++ src/pages/home/HomeScreen.tsx
@@ -1,14 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
 
-export default function HomeScreen() {
+
+const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
+    const handleNavigate = () => {
+        navigation.navigate('PlansScreen' as never);
+    }
+
     return (
         <View>
             <Text>HomeScreen</Text>
             <Text>HomeScreen</Text>
             <Text>HomeScreen</Text>
-            <Text>HomeScreen</Text>
+            <Text>HomeScreen2</Text>
+            <Button title='next' onPress={handleNavigate} />
 
         </View>
     )
 }
+
+export default HomeScreen;
--- src/pages/stack/HomeStack.tsx
+++ src/pages/stack/HomeStack.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from '../home/HomeScreen'
+import PlansScreen from '../plans/PlansScreen'
 
 const HomeStack = () => {
     const Stack = createNativeStackNavigator();
@@ -16,7 +17,7 @@ const HomeStack = () => {
                 }}
             >
                 <Stack.Screen name="HomeScreen" component={HomeScreen} />
-                <Stack.Screen name="PlansScreen" component={HomeScreen} />
+                <Stack.Screen name="PlansScreen" component={PlansScreen} />
                 <Stack.Screen name="ResumeScreen" component={HomeScreen} />
             </Stack.Navigator>
             {/* <AppStack /> */}
